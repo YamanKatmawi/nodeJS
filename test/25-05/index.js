c3c39b508905dff220c9e33b5688a312cf699164
YamanKatmawi <yamankatmawi@gmx.de>
2020-05-25 14:19:13 +0200

    creat New text throw nodeJs

---
+++ test/25-05/index.js
@@ -0,0 +1,13 @@
+console.log(process);
+// process.argv.forEach((item, i) => {
+//   console.log(`${i} : ${item}`);
+// });
+// const args = process.argv.slice(2);
+// console.log(args);
+// console.log(`hey ${args[0]}`);
+const fs = require("fs");
+fs.readFile("./longText.txt", "utf8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+console.log("hole");
