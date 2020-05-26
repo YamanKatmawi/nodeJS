582619a824a851613be87394af4f1ae10c1d7262
YamanKatmawi <yamankatmawi@gmx.de>
2020-05-26 10:39:09 +0200

    file system

---
+++ 26-5/index.js
@@ -0,0 +1,35 @@
+// console.log(process);
+// const print = require("./stars");
+const args = process.argv.slice(2);
+// print(args[0], args[1]);
+function parseNum(arg) {
+  const number = parseFloat(arg);
+  if (isNaN(number)) {
+    console.log(`Sorry, ${arg} is not number. please try again`);
+    process.exit();
+  }
+  return number;
+}
+function sum(numArray) {
+  return numArray.reduce((sum, n) => sum + n, 0);
+}
+function avg(numArray) {
+  return sum(numArray) / numArray.length;
+}
+const [operation, ...rest] = args;
+
+const numbers = rest.map(parseNum);
+//sum 3 5 6
+//avg 45 6 67
+switch (operation) {
+  case "sum":
+    console.log(sum(numbers));
+    break;
+  case "avg":
+    console.log(avg(numbers));
+    break;
+  default:
+    console.log(
+      "I cannot do this, please type sum to calculate Sum or avg to calculate Avg"
+    );
+}
---
+++ 26-5/stars.js
@@ -0,0 +1,3 @@
+module.exports = (stars = 10, header = "hey") => {
+  console.log(`${"*".repeat(stars)} \n ${header} \ ${"*".repeat(stars)}`);
+};
